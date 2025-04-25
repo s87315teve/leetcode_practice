df5c6b3b33a8c3345ee05eec16a6af7f82db4b4e
邱佳詮 <chiu@chiu-macmini.local>
2025-04-25 14:04:56 +0800

    update day1

---
+++ easy/day1-two_sum/answer.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_dict = {}  # 建立一個字典來儲存已經遍歷過的數字及其索引
+    
+        for i, num in enumerate(nums):
+            complement = target - num  # 計算與當前數字匹配的另一個數字
+            
+            # 檢查這個匹配的數字是否已經在字典中
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            
+            # 將當前數字和索引加入字典
+            num_dict[num] = i
+
+        
